5d26bb7eaec27455ef848b7eb4883de5c8d321d4
RubLuc <120739396+RubLuc@users.noreply.github.com>
2022-12-16 17:36:57 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,25 +1,5 @@
-# Installing Go
+# Installing Git
 sudo apt-get install git
+
+# Installing Go
 sudo apt-get install golang
-
-# Copying Custom Modules
-sudo mkdir $HOME/go/src/CXplorer
-sudo cp -r modules/ $HOME/go/src/CXplorer
-
-# Installing Required C Libraries
-sudo apt-get install libpcap-dev
-
-# Installing Required Go Packages
-go get github.com/google/gopacket
-go get golang.org/x/net/bpf
-go get golang.org/x/sys/unix
-
-# Copying Wordlist
-sudo mkdir /usr/local/CXplorer
-sudo cp wordlist.txt /usr/local/CXplorer/
-
-# Building Tool
-go build -o CXplorer
-
-# Moving Executable to Bin
-sudo cp CXplorer /usr/local/bin/
